0e3dd4c06128429bd50b57d2320e4288ce37f7fa
Doxi-i <84279785+Doxi-i@users.noreply.github.com>
2023-04-24 11:54:54 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="footer">
+    <div class="footer-content">
+        <div class="footer-left">
+            <div class="logo-left">
+                <img src="img/logo.svg" alt="">
+            </div>
+            <div class="tekst-right">
+                <p>
+                    Plivački klub Novi Zagreb <br/>
+                    Kombolova 4 a, 10010 Zagreb <br/>
+                    Fax: 01/ 6521 912 <br/>
+                    E-mail: swimzg@swimzg.hr <br/>
+                </p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <a href="https://www.facebook.com/PlivackiKlubNoviZagreb/" class="facebook"><button>posjetite nas na facebooku</button></a>
+        </div>
+    </div>
+</div>
+<div class="footer-rights">
+    <div class="rights">
+        <p>© 2023 Sva prava Plivački kub Novi Zagreb</p> 
+        <a href="#" class="politika">Politika privatnosti</a>
+    </div>
+</div>
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
